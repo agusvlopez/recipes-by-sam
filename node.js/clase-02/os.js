2f99621c942e5806f504687d244ea387047e2fdf
agusvlopez <agusvlopez@gmail.com>
2023-08-21 00:00:49 -0300

    add: create os.js to import os module

---
+++ node.js/clase-02/os.js
@@ -0,0 +1,9 @@
+//importamos el modulo os (sacado de la pagina nodejs)...
+
+import os from 'node:os';
+
+console.log(os.version());
+console.log(os.release());
+console.log(os.cpus());
+console.log(os.arch());
+console.log(os.platform());
