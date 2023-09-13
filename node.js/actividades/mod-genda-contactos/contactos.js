f738ab5abef70e914d4adbc6dd6e9dfbc0ba4d35
agusvlopez <agusvlopez@gmail.com>
2023-09-13 13:29:20 -0300

    add: add contacts to contactos.csv

--- node.js/actividades/mod-genda-contactos/contactos.js
+++ node.js/actividades/mod-genda-contactos/contactos.js
@@ -1,10 +1,13 @@
 //importo el modulo validaciones
 import {validarTelefono, validarEmail} from './validaciones.js';
+//importo el fs
+import fs from 'node:fs';
 
 let listaContactos = [];
+let csv;
 
 function agregarContacto(nombre, telefono, email) {
-    
+
     let contactoNuevo = {
         nombre: "",
         telefono: "",
@@ -26,9 +29,45 @@ function agregarContacto(nombre, telefono, email) {
     }
 }
 
+function exportarCSV(listaContactos){
+    csv = "Nombre,Telefono,Email\n"; // Encabezados CSV
+
+    // for (contacto of listaContactos) {
+    //   csv += `${contacto.nombre},${contacto.telefono},${contacto.email}\n`;
+    // }
+    listaContactos.forEach((contacto) => {
+        
+        csv += `${contacto.nombre},${contacto.telefono},${contacto.email}\n`;
+    });
+    console.log(csv)
+    return csv;
+}
+
+
+function guardarCSVEnArchivo(nombreArchivo, csvData) {
+
+  fs.writeFile(nombreArchivo, csvData, {encoding: 'utf-8', flag: 'w'},
+  function(err){
+    if(err){
+        console.log("No se pudo guardar");
+    }else {
+        console.log(`Archivo "${nombreArchivo}" guardado exitosamente.`);
+    }
+    });
+ 
+}
+
+// Llamada a la función para generar el CSV y guardarlo en un archivo
+
 
 agregarContacto("Brian Lara","123-456-7890","blara@email.com");
 agregarContacto("Juan Gonzalez", "123-456-7899", "jgonzalez@email.com");
 console.log(listaContactos);
 agregarContacto("Error No Funciona", "000000", "email-invalido");
 console.log(listaContactos);
+
+const csvData = exportarCSV(listaContactos);
+guardarCSVEnArchivo("contactos.csv", csvData);
+
+exportarCSV(listaContactos);
+console.log(csv)
