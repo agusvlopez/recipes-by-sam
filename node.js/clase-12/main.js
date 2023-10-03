59604089369a533c3c35847f4d037bc79957a0f4
agusvlopez <agusvlopez@gmail.com>
2023-10-03 20:53:29 -0300

    add: iniciando con mongoDB

---
+++ node.js/clase-12/main.js
@@ -0,0 +1,39 @@
+//creo el cliente:
+
+import { MongoClient, ObjectId } from 'mongodb';
+
+//en el parametro pongo la URI o connection string(sin poner localhost)
+const client = new MongoClient('mongodb://127.0.0.1:27017');
+
+//funcion asincronica:
+client.connect()
+.then(async function(){
+    console.log("Conectado al servidor");
+
+    const db = client.db('test');
+
+    // db.collection('Users').insertOne({
+    //     name: 'Mongo User',
+    //     type: 'System'
+    // })
+
+    //Devuelve una promesa con un documento
+    // const doc = await db.collection("Users")
+    // .findOne({_id: new ObjectId('651c964f5941071a2289cf6c')});
+    // console.log(doc);
+
+
+    const cursor = db.collection("Users").find();
+
+    const docs = await db.collection("Users").find({type: "Alumno"}).toArray();
+    
+    const update = await db.collection("Users").updateOne({_id: new ObjectId('651ca3b72574213f2e6d4cef')}, {
+        $set: {
+            name: "Usuario nuevo"
+        }
+    })
+
+})
+.catch(function(err){
+    console.log("No se pudo conectar: ", err);
+})
