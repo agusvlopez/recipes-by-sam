debf4cccad386a0283560ed6d9a59c9ffbefe008
agusvlopez <agusvlopez@gmail.com>
2024-01-26 00:15:32 -0300

    add: filter in all products page

--- react/clase-01/react-app/src/pages/admin/AllProductsPage.jsx
+++ react/clase-01/react-app/src/pages/admin/AllProductsPage.jsx
@@ -6,9 +6,10 @@ import { useGetProductsQuery } from "../../features/apiSlice";
 
 
 function AdminProductsPage({ }) {
-
     const { data: products, isLoading } = useGetProductsQuery();
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [filteredProducts, setFilteredProducts] = useState(products);
 
     const location = useLocation();
 
@@ -31,6 +32,20 @@ function AdminProductsPage({ }) {
         });
     };
 
+    const handleQuery = (e) => {
+        setSearchQuery(e.target.value);
+    }
+
+    const handleSearch = () => {
+        const filtered = searchQuery.trim() !== "" ?
+            products?.filter(product =>
+                product.name.toLowerCase().includes(searchQuery.toLowerCase())
+            ) : products;
+
+        setFilteredProducts(filtered);
+    };
+
+
     return (
         <>
             <div className="container mx-auto mt-8 p-4">
@@ -43,11 +58,26 @@ function AdminProductsPage({ }) {
                 <div className="mb-6">
                     <Link to={'./newProduct'}><button className="p-2 text-white rounded">Add a product +</button></Link>
                 </div>
+                <div className="flex mb-4 max-w-md">
+                    <input
+                        type="text"
+                        placeholder="Search by title..."
+                        value={searchQuery}
+                        onChange={handleQuery}
+                        className="p-2 border rounded-l-md flex-grow"
+                    />
+                    <button
+                        onClick={handleSearch}
+                        className="p-2 text-white rounded-r-md"
+                    >
+                        Search
+                    </button>
+                </div>
                 {isLoading ? (
                     <Loader />
                 ) : (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        {products?.map((product) => (
+                        {filteredProducts?.map((product) => (
                             <Link to={`./${product._id}`} key={product._id}>
                                 <div key={product._id} className="bg-white p-4 rounded-md shadow-md">
                                     <img src={product.file} alt="" />
