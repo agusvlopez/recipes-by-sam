3b8ab55f1fb918b3d1c129032c061e496bf18e76
agusvlopez <agusvlopez@gmail.com>
2024-03-19 23:58:36 -0300

    add: style in admin

--- react/clase-01/react-app/src/pages/admin/AllProductsPage.jsx
+++ react/clase-01/react-app/src/pages/admin/AllProductsPage.jsx
@@ -77,7 +77,7 @@ function AdminProductsPage({ }) {
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         {filteredProducts?.map((product) => (
                             <Link to={`./${product._id}`} key={product._id}>
-                                <div key={product._id} className="bg-white p-4 rounded-md shadow-md">
+                                <div key={product._id} className="bg-white p-4 rounded-md shadow-md product-card--admin">
                                     <img src={product.file} alt="" />
                                     <h3 className="text-xl font-bold mb-2 mt-2">{product.name}</h3>
                                     <p>{product.description}</p>
